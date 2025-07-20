418adde98e817baf3760611a2186fcd9603bcbf7
Njabu M. <32612033+GyL-nm@users.noreply.github.com>
2025-07-21 00:05:36 +0100

    batch file for restarting rainmeter and livelywallpaper

---
+++ bg.bat
@@ -0,0 +1,4 @@
+taskkill /f /im "Lively.exe"
+taskkill /f /im "Rainmeter.exe"
+start "" "C:\Users\njabu\AppData\Local\Programs\Lively Wallpaper\Lively.exe"
+start "" "C:\Program Files\Rainmeter\Rainmeter.exe" !LoadLayout "[Current]2"
