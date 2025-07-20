c82d51dac08a6bf1c6b183be75e4ce1cebdf2ff4
Njabu M. <32612033+GyL-nm@users.noreply.github.com>
2025-07-21 00:06:22 +0100

    Delete bg.bat

--- bg.bat
+++
@@ -1,4 +0,0 @@
-taskkill /f /im "Lively.exe"
-taskkill /f /im "Rainmeter.exe"
-start "" "C:\Users\njabu\AppData\Local\Programs\Lively Wallpaper\Lively.exe"
-start "" "C:\Program Files\Rainmeter\Rainmeter.exe" !LoadLayout "[Current]2"
